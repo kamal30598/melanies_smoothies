15840e352e1de5309c10c835946b0c36405e2502
kamal30598 <kamal.sharma@grazitti.com>
2025-08-01 18:26:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,8 @@ if Ingredients_list:
 
     for fruit_chosen in Ingredients_list:
         ingredients_string +=fruit_chosen +' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information ')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" +fruit_chosen)
         #st.text(smoothiefroot_response.json())
         st_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)  
 
